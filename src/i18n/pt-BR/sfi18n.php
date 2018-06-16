1e6682b1e16aaa70602b24717f6c68f80fd24bd9
Sidney Lins <slinstj@gmail.com>
2018-06-16 02:46:46 -0300

    Translates form grades into pt-BR

--- src/i18n/pt-BR/sfi18n.php
+++ src/i18n/pt-BR/sfi18n.php
@@ -2,6 +2,10 @@
 return [
     'Grade' => 'Nota',
     'Comment' => 'Comentário',
-    'Error: the widget instance must be passed via constructor to SimpleFeedback Model.'
-        => 'Erro: a instância do widget deve ser passada para o Model SimpleFeedback via constructor.'
+    'Error: the widget instance must be passed via constructor to SimpleFeedback Model.' => 'Erro: a instância do widget deve ser passada para o Model SimpleFeedback via constructor.',
+    'The worst!' => 'O pior!',
+    'Bad' => 'Ruim',
+    'Regular' => 'Regular',
+    'Good' => 'Bom',
+    'Excellent!' => 'Excelente',
 ];
