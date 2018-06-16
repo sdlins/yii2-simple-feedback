84b20c3a5f5cf39631933f62774ee095cc762607
Sidney Lins <slinstj@gmail.com>
2018-06-16 02:04:30 -0300

    Adds pt-BR translations

---
+++ src/i18n/pt-BR/sfi18n.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'Grade' => 'Nota',
+    'Comment' => 'Comentário',
+    'Error: the widget instance must be passed via constructor to SimpleFeedback Model.'
+        => 'Erro: a instância do widget deve ser passada para o Model SimpleFeedback via constructor.'
+];
