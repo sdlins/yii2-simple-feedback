0cb9cae055425b27db4a74a5a4563d02bcfe5fa6
Sidney Lins <slinstj@gmail.com>
2018-06-16 20:41:58 -0300

    Check if the fields should be available

--- src/views/feedback.php
+++ src/views/feedback.php
@@ -35,35 +35,37 @@ $this->registerCss($css);
     }
 </script>
 <?php $form = ActiveForm::begin() ?>
-    <?= $form->field($model, $this->context->dbGradeField, [
-        'options' => [
-            'class' => '',
-        ],
-        'errorOptions' => [
-            'tag' => null,
-        ],
-    ])->hiddenInput([
-        'id' => 'grade',
-        'name' => 'SimpleFeedbackModel[grade]',
-        'value' => 0,
-    ]) ?>
-    <ul class="sf">
-        <li class="sf-star" onclick="sfUpdateGrade(this)" data-grade="1">
-            <i class="glyphicon glyphicon-star"></i>
-        </li>
-        <li class="sf-star" onclick="sfUpdateGrade(this)" data-grade="2">
-            <i class="glyphicon glyphicon-star"></i>
-        </li>
-        <li class="sf-star" onclick="sfUpdateGrade(this)" data-grade="3">
-            <i class="glyphicon glyphicon-star"></i>
-        </li>
-        <li class="sf-star" onclick="sfUpdateGrade(this)" data-grade="4">
-            <i class="glyphicon glyphicon-star"></i>
-        </li>
-        <li class="sf-star" onclick="sfUpdateGrade(this)" data-grade="5">
-            <i class="glyphicon glyphicon-star"></i>
-        </li>
-    </ul>
-    <?= $form->field($model, $this->context->dbCommentField)->textarea() ?>
+    <?php if ($this->context->isGradeAvailable): ?>
+        <?= $form->field($model, $this->context->dbGradeField, [
+            'options' => [
+                'class' => '',
+            ],
+            'errorOptions' => [
+                'tag' => null,
+            ],
+        ])->hiddenInput([
+            'id' => 'grade',
+            'name' => 'SimpleFeedbackModel[grade]',
+            'value' => 0,
+        ]) ?>
+        <ul class="sf">
+            <li class="sf-star" onclick="sfUpdateGrade(this)" data-grade="1">
+                <i class="glyphicon glyphicon-star"></i>
+            </li>
+            <li class="sf-star" onclick="sfUpdateGrade(this)" data-grade="2">
+                <i class="glyphicon glyphicon-star"></i>
+            </li>
+            <li class="sf-star" onclick="sfUpdateGrade(this)" data-grade="3">
+                <i class="glyphicon glyphicon-star"></i>
+            </li>
+            <li class="sf-star" onclick="sfUpdateGrade(this)" data-grade="4">
+                <i class="glyphicon glyphicon-star"></i>
+            </li>
+            <li class="sf-star" onclick="sfUpdateGrade(this)" data-grade="5">
+                <i class="glyphicon glyphicon-star"></i>
+            </li>
+        </ul>
+    <?php endif ?>
+    <?= $this->context->isCommentAvailable ? $form->field($model, $this->context->dbCommentField)->textarea() : '' ?>
     <?= $form->field($model, $this->context->dbTargetField)->hiddenInput()->label(false) ?>
 <?php ActiveForm::end() ?>
