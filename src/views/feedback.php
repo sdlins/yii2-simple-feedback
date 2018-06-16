e0e57fef6c4e983cba3a463f55f8544a29dff892
Sidney Lins <slinstj@gmail.com>
2018-06-16 02:43:17 -0300

    Creates a very basic form

--- src/views/feedback.php
+++ src/views/feedback.php
@@ -1,9 +1,17 @@
 <?php
 
 use yii\widgets\ActiveForm;
+use slinstj\widgets\SimpleFeedback;
 
-/* @var SimpleFeedback $model */
 ?>
-<?= $form = ActiveForm::begin() ?>
-    <h1>E aí pessoal!</h1>
-<?= ActiveForm::end() ?>
+<?php $form = ActiveForm::begin() ?>
+    <?= $form->field($model, $this->context->dbGradeField)->dropDownList([
+        1 => \Yii::t('sfi18n', 'The worst!'),
+        2 => \Yii::t('sfi18n', 'Bad'),
+        3 => \Yii::t('sfi18n', 'Regular'),
+        4 => \Yii::t('sfi18n', 'Good'),
+        5 => \Yii::t('sfi18n', 'Excellent!'),
+    ]) ?>
+    <?= $form->field($model, $this->context->dbCommentField)->textarea() ?>
+    <?= $form->field($model, $this->context->dbTargetField)->hiddenInput()->label(false) ?>
+<?php ActiveForm::end() ?>
