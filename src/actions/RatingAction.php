0a56809f117a5a7db69fe0d4b618cc6c91ffa393
Sidney Lins <slinstj@gmail.com>
2018-06-17 02:10:22 -0300

    Adds standalone action for rating

---
+++ src/actions/RatingAction.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace slinstj\widgets\SimpleFeedback\actions;
+
+use yii\base\Action;
+use slinstj\widgets\SimpleFeedback\models\SimpleFeedbackModel;
+
+class RatingAction extends Action
+{
+    public function run()
+    {
+        $model = new SimpleFeedbackModel;
+        $returnUrl = $_POST['sfReferrer'] ?? \Yii::$app->request->referrer ?? \Yii::$app->homeUrl;
+        $returnUrl .= (strpos($returnUrl, '?') === false ? '?' : '&');
+
+        if ($model->load(\Yii::$app->request->post()) && $model->save()) {
+            $returnUrl .= 'sfResponse=' . urlencode(\Yii::t('app', 'Thanks for your rating!'));
+            $returnUrl .= '&sfResponseType=success';
+        } else {
+            $returnUrl .= 'sfResponse=' . urlencode(\Yii::t('app', 'Sorry! Your rating could not be saved at this moment.'));
+            $returnUrl .= '&sfResponseType=danger';
+        }
+        return $this->controller->redirect($returnUrl);
+    }
+}
