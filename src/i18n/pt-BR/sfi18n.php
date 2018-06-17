6534645efc9e7dbe14a4782d390205856fb26ee3
Sidney Lins <slinstj@gmail.com>
2018-06-17 00:58:00 -0300

    Removes translations since it will be up to app

--- src/i18n/pt-BR/sfi18n.php
+++ src/i18n/pt-BR/sfi18n.php
@@ -1,11 +1,3 @@
 <?php
 return [
-    'Grade' => 'Nota',
-    'Comment' => 'Comentário',
-    'Error: the widget instance must be passed via constructor to SimpleFeedback Model.' => 'Erro: a instância do widget deve ser passada para o Model SimpleFeedback via constructor.',
-    'The worst!' => 'O pior!',
-    'Bad' => 'Ruim',
-    'Regular' => 'Regular',
-    'Good' => 'Bom',
-    'Excellent' => 'Excelente',
 ];
