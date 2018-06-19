a464dbf5c07846388a3bd6414092d4821eafa765
Sidney Lins <slinstj@gmail.com>
2018-06-19 01:16:36 -0300

    Adds pt-BR translations

--- src/i18n/pt-BR/sfi18n.php
+++ src/i18n/pt-BR/sfi18n.php
@@ -1,3 +1,8 @@
 <?php
 return [
+    'Thanks for your feedback!' => 'Obrigado por seu feedback!',
+    'Sorry! Your feedback could not be saved at this moment.' => 'Desculpe! Seu feedback não pôde ser salvo no momento.',
+    'Submit' => 'Enviar',
+    'Rating' => 'Avaliação',
+    'Comment' => 'Comentário',
 ];
